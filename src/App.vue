971168be27e73cecc41e30ce015b84d8e88a3ee1
andoshin11 <shinglish11@gmail.com>
2018-10-20 16:18:48 +0900

    First Vue app!

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    Vue Rendered!
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: red;
+}
+</style>
