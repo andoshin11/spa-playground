438dd86b19934c5325151861c53145e8f8b9f660
andoshin11 <shinglish11@gmail.com>
2018-10-20 23:06:10 +0900

    Fix webpack alias

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,16 @@
-const path = require("path")
-const HtmlWebpackPlugin = require("html-webpack-plugin")
-const VueLoaderPlugin = require("vue-loader/lib/plugin")
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
 // Constants
-const MODE = process.env.NODE_ENV || "development"
-const DEV = MODE === "development"
+const MODE = process.env.NODE_ENV || 'development'
+const DEV = MODE === 'development'
 
-const SRC = path.join(__dirname, "src")
+const SRC = path.join(__dirname, 'src')
 
 const plugins = [
   new HtmlWebpackPlugin({
-    template: "./public/index.html"
+    template: './public/index.html'
   }),
   new VueLoaderPlugin()
 ]
@@ -20,44 +20,42 @@ module.exports = {
   entry: {
     main: './src/index.ts'
   },
-  devtool: "cheap-module-eval-source-map",
+  devtool: 'cheap-module-eval-source-map',
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js"
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
   },
   module: {
     rules: [
       {
         test: /\.vue$/,
-        loader: "vue-loader",
+        loader: 'vue-loader'
       },
       {
         test: /\.ts$/,
         use: [
           {
-            loader: "ts-loader",
+            loader: 'ts-loader',
             options: {
-              appendTsSuffixTo: [
-                '\\.vue$'
-              ]
+              appendTsSuffixTo: ['\\.vue$']
             }
           }
         ],
         exclude: /node_modules/
       },
-      { test: /\.css$/, loader: 'style-loader!css-loader' },
+      { test: /\.css$/, loader: 'style-loader!css-loader' }
     ]
   },
   plugins,
   resolve: {
-    extensions: [".ts", ".js", ".vue"],
+    extensions: ['.ts', '.js', '.vue'],
     alias: {
-      "@": "./src",
-      "vue$": "vue/dist/vue.esm.js"
+      '@': path.join(__dirname, 'src'),
+      vue$: 'vue/dist/vue.esm.js'
     }
   },
   devServer: {
-    contentBase: path.join(__dirname, "dist"),
+    contentBase: path.join(__dirname, 'dist'),
     port: 8080
   }
 }
