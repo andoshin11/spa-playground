99603f9cc8f802c7267d7371573c5a0fb4200ccf
andoshin11 <shinglish11@gmail.com>
2018-10-20 16:17:51 +0900

    Remove test files

--- src/main.ts
+++
@@ -1,8 +0,0 @@
-import { uppercase } from './utils'
-
-type Hello = "hello" | "sup"
-
-const greeting = (word: Hello) => console.log(`greeting: ${uppercase(word)}.`)
-
-greeting('hello')
-greeting("sup")
