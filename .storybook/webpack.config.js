f5d8e83cd55150d0ba457ecbf6c199ed1db06162
andoshin11 <shinglish11@gmail.com>
2018-10-21 02:58:11 +0900

    Setup Storybook

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path')
+module.exports = (baseConfig, env, config) => {
+  config.module.rules.push({
+    test: /\.ts$/,
+    use: [
+      {
+        loader: 'ts-loader',
+        options: {
+          appendTsSuffixTo: ['\\.vue$']
+        }
+      }
+    ],
+    exclude: /node_modules/
+  })
+  config.resolve.extensions.push('.ts')
+  return config
+}
