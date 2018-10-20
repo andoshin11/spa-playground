22b670bc8180018e49921e6d6e7b2bd7ee160f09
andoshin11 <shinglish11@gmail.com>
2018-10-20 16:18:38 +0900

    Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path")
 const HtmlWebpackPlugin = require("html-webpack-plugin")
+const VueLoaderPlugin = require("vue-loader/lib/plugin")
 
 // Constants
 const MODE = process.env.NODE_ENV || "development"
@@ -10,31 +11,39 @@ const SRC = path.join(__dirname, "src")
 const plugins = [
   new HtmlWebpackPlugin({
     template: "./public/index.html"
-  })
+  }),
+  new VueLoaderPlugin()
 ]
 
 module.exports = {
   mode: MODE,
   entry: {
-    main: './src/main.ts'
+    main: './src/index.ts'
   },
   devtool: "inline-source-map",
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "[name].js"
+    filename: "bundle.js"
   },
   module: {
     rules: [
       {
+        test: /\.vue$/,
+        loader: "vue-loader",
+      },
+      {
         test: /\.ts$/,
         use: "ts-loader",
         exclude: /node_modules/
-      }
+      },
+      { test: /\.css$/, loader: 'style-loader!css-loader' },
     ]
   },
   plugins,
   resolve: {
-    extensions: [".ts", ".js", ".vue"]
+    extensions: [".ts", ".js", ".vue"],
+    alias: {
+      "vue$": "vue/dist/vue.esm.js"
+    }
   }
 }
-
