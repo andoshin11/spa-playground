e04991c719bf54c6e92284aa8a6a0d6c9c71855c
andoshin11 <shinglish11@gmail.com>
2018-10-20 19:12:57 +0900

    Add variables.css

--- src/App.vue
+++ src/App.vue
@@ -6,11 +6,14 @@
 </template>
 
 <style>
+@import './assets/css/variables.css';
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: red;
+  color: var(--color-blue);
+  font-size: var(--fontSize-h1);
 }
 </style>
