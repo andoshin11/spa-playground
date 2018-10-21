0a84efe96b960417f90661557757050e869ef7a5
andoshin11 <shinglish11@gmail.com>
2018-10-21 20:52:50 +0900

    Add @storybook/addon-a11y

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,9 @@
-import { configure } from '@storybook/vue'
+import { configure, addDecorator } from '@storybook/vue'
+import { checkA11y } from '@storybook/addon-a11y'
 
 const req = require.context('../src', true, /\.story\.ts$/)
+
+addDecorator(checkA11y)
 
 function loadStories() {
   req.keys().forEach(filename => req(filename))
