35fd252efccb52a3efbd43210ad75394ee4015ab
andoshin11 <shinglish11@gmail.com>
2018-10-21 17:15:44 +0900

    Add Disable Buttons story

--- src/components/Base/Button.story.ts
+++ src/components/Base/Button.story.ts
@@ -3,9 +3,10 @@ import { storiesOf } from '@storybook/vue'
 
 import Button from './Button.vue'
 
-storiesOf('components/Base/Button', module).add('Default', () => ({
-  components: { Button },
-  template: `
+storiesOf('components/Base/Button', module)
+  .add('Default', () => ({
+    components: { Button },
+    template: `
   <div class="Storybook">
     <div class="StorybookSection">
       <div class="StorybookSection__Header">Primary Buttons</div>
@@ -19,4 +20,21 @@ storiesOf('components/Base/Button', module).add('Default', () => ({
       </div>
   </div>
 `
-}))
+  }))
+  .add('Disabled', () => ({
+    components: { Button },
+    template: `
+  <div class="Storybook">
+    <div class="StorybookSection">
+      <div class="StorybookSection__Header">Primary Buttons</div>
+      <div class="StorybookSection__Body">
+        <div class="StorybookSection__Label">Large</div>
+        <Button text="test" size="large" disabled />
+        <div class="StorybookSection__Label">Midium</div>
+        <Button text="test" disabled />
+        <div class="StorybookSection__Label">Small</div>
+        <Button text="test" size="small" disabled />
+      </div>
+  </div>
+`
+  }))
