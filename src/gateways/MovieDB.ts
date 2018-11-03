c8c61f2ae4f43846cdb5db74dae50ffff9b959eb
andoshin11 <shinglish11@gmail.com>
2018-11-03 11:58:52 +0900

    :tada: Separate gateway

---
+++ src/gateways/MovieDB.ts
@@ -0,0 +1,13 @@
+import { APIClient } from '@/infra/network/APIClient'
+import { GetPopularMovies, GetGenres } from '@/infra/network/api/Movie'
+import { IMovieProps, Genre } from '@/entities/Movie'
+
+export default class MovieDBGateway {
+  async fetchPopularMovies(): Promise<IMovieProps[]> {
+    return await APIClient.shared.request(new GetPopularMovies({}))
+  }
+
+  async fetchGenres(): Promise<Genre[]> {
+    return await APIClient.shared.request(new GetGenres())
+  }
+}
