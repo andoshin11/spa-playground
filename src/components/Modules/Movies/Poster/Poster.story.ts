429fcdcb325c2fbe2662b0dab88306ec7893ed2f
andoshin11 <shinglish11@gmail.com>
2018-11-18 00:12:31 +0900

    Wait for image loaded

--- src/components/Modules/Movies/Poster/Poster.story.ts
+++ src/components/Modules/Movies/Poster/Poster.story.ts
@@ -1,4 +1,5 @@
 import { storiesOf } from '@storybook/vue'
+import { withScreenshot } from 'zisui'
 
 import Poster from './Poster.vue'
 import { MovieFactory } from '@/entities/Movie'
@@ -6,6 +7,11 @@ import { MovieFactory } from '@/entities/Movie'
 const dummyMovie = MovieFactory()
 
 storiesOf('components/Modules/Movies/Poster', module)
+  .addDecorator(
+    withScreenshot({
+      delay: 3000
+    })
+  )
   .add('Default', () => ({
     components: { Poster },
     data() {
