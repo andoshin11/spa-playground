0327328141f7d8c97150fa0fd5ec81d557129d8a
andoshin11 <shinglish11@gmail.com>
2018-11-17 10:45:08 +0900

    Update jest settings

--- jest.config.snapshot.js
+++ jest.config.snapshot.js
@@ -2,26 +2,15 @@ module.exports = {
   moduleFileExtensions: ['vue', 'js', 'ts', 'json'],
   moduleDirectories: ['node_modules'],
   transform: {
-    '^.+\\.js$': 'ts-jest',
-    '^.+\\.ts$': 'ts-jest',
-    '.*\\.(vue)$': '<rootDir>/node_modules/jest-vue-preprocessor'
+    '^.+\\.(js|ts)$': '<rootDir>/node_modules/babel-jest',
+    '.*\\.(vue)$': '<rootDir>/.jest/babel-vue-jest'
   },
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
-    '[^/]vue$': 'vue/dist/vue.common.js',
     '^@storybook/(.*)$': '<rootDir>/node_modules/@storybook/$1'
   },
   snapshotSerializers: ['jest-serializer-vue'],
   testMatch: ['<rootDir>/test/snapshot/*.js'],
   verbose: true,
-  setupFiles: ['<rootDir>/.jest/register-context.js'],
-  transformIgnorePatterns: [
-    './node_modules/(?!vue)',
-    './node_modules/@storybook'
-  ],
-  globals: {
-    'ts-jest': {
-      babelConfig: '.babelrc'
-    }
-  }
+  setupFiles: ['<rootDir>/.jest/register-context.js']
 }
