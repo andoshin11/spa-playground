baf86a4ba8861427275a5faf7ce53a967312f1f1
andoshin11 <shinglish11@gmail.com>
2018-11-17 10:28:18 +0900

    Init movie container

---
+++ src/containers/Movie/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="Movie" />
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+import Presenter, { IPresenter } from './presenter'
+
+// Use Case
+
+// Repositories
+
+// Service
+
+// components
+
+interface IData {}
+
+export default Vue.extend({
+  props: {
+    id: {
+      type: String,
+      required: true
+    }
+  },
+  data(): IData {
+    return {}
+  },
+  methods: {
+    async fetchMovie() {
+      const id = parseInt(this.id, 10)
+    }
+  }
+})
+</script>
