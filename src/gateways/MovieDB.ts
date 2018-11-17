b945d186a266802d966d1e289d878e58234e24c1
andoshin11 <shinglish11@gmail.com>
2018-11-17 10:18:28 +0900

    :+1: Add fetchMovie method

--- src/gateways/MovieDB.ts
+++ src/gateways/MovieDB.ts
@@ -1,10 +1,14 @@
 import { APIClient } from '@/infra/network/APIClient'
-import { GetPopularMovies, GetGenres } from '@/infra/network/api/Movie'
+import { GetPopularMovies, GetGenres, GetMovie } from '@/infra/network/api/Movie'
 import { IMovieProps, Genre } from '@/entities/Movie'
 
 export default class MovieDBGateway {
   async fetchPopularMovies(): Promise<IMovieProps[]> {
     return await APIClient.shared.request(new GetPopularMovies({}))
+  }
+
+  async fetchMovie(id: number): Promise<IMovieProps> {
+    return await APIClient.shared.request(new GetMovie(id))
   }
 
   async fetchGenres(): Promise<Genre[]> {
